9e355a2bb86d0d9ac5db0d6708730531d6f1198f
Darren Garnier <dgarnier@reinrag.net>
2020-07-11 08:52:56 -0400

    cleaning up requirements and looking at mysql database

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+import sqlalchemy.types as types
+import jsonpickle
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
@@ -13,6 +15,20 @@ class SQLA(SQLAlchemy):
 
 
 db = SQLA()
+
+
+class Json(types.MutableType, types.TypeDecorator):
+    impl = types.Unicode
+
+    def process_bind_param(self, value, engine):
+        return unicode(jsonpickle.encode(value))
+
+    def process_result_value(self, value, engine):
+        if value:
+            return jsonpickle.decode(value)
+        else:
+            # default can also be a list
+            return {}
 
 
 class Athlete(db.Model, UserMixin):
@@ -31,7 +47,7 @@ class Athlete(db.Model, UserMixin):
     access_token_expires_at = db.Column(db.Integer)
     refresh_token = db.Column(db.String)
     created_date = db.Column(db.DateTime, default=datetime.utcnow)
-    last_updated = db.Column(db.DateTime, default=datetime.utcnow)
+    last_updated = db.Column(db.DateTime, onupdate=datetime.utcnow)
     last_activity_check = db.Column(db.DateTime, default=datetime.utcnow)
 
     @property
@@ -111,6 +127,7 @@ class Activity(db.Model):
     manual = db.Column(db.Boolean)
     private = db.Column(db.Boolean)
     flagged = db.Column(db.Boolean)
+    
     last_updated = db.Column(db.DateTime, default=datetime.utcnow)
 
 
