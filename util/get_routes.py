a112f8bbff5295c12acc9e5ff0e2a2177e286d19
Darren Garnier <dgarnier@reinrag.net>
2020-07-12 22:35:23 -0400

    now with the right route numbers

---
+++ util/get_routes.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import os
+
+routes = [
+    ('_B_lackLivesMatter', "https://ridewithgps.com/routes/32978116"),
+    ('B_l_ackLivesMatter', "https://ridewithgps.com/routes/32978288"),
+    ('Bl_a_ckLivesMatter', "https://ridewithgps.com/routes/32979113"),
+    ('Bla_c_kLivesMatter', "https://ridewithgps.com/routes/33078801"),
+    ('Blac_k_LivesMatter', "https://ridewithgps.com/routes/33078890"),
+    ('Black_L_ivesMatter', "https://ridewithgps.com/routes/33176643"),
+    ('BlackL_i_vesMatter', "https://ridewithgps.com/routes/32979727"),
+    ('BlackLi_v_esMatter', "https://ridewithgps.com/routes/33076433"),
+    ('BlackLiv_e_sMatter', "https://ridewithgps.com/routes/33076537"),
+    ('BlackLive_s_Matter', "https://ridewithgps.com/routes/33076805"),
+    ('BlackLives_M_atter', "https://ridewithgps.com/routes/32977936"),
+    ('BlackLivesM_a_tter', "https://ridewithgps.com/routes/33076918"),
+    ('BlackLivesMa_t_ter', "https://ridewithgps.com/routes/33077009"),
+    ('BlackLivesMat_t_er', "https://ridewithgps.com/routes/33077220"),
+    ('BlackLivesMatt_e_r', "https://ridewithgps.com/routes/33077727"),
+    ('BlackLivesMatte_r_', "https://ridewithgps.com/routes/33077542"),
+]
+
+
+routes = [
+    ("_B_lackLivesMatter", "https://ridewithgps.com/routes/32977936"), 
+    ("B_l_ackLivesMatter", "https://ridewithgps.com/routes/32978116"), 
+    ("Bl_a_ckLivesMatter", "https://ridewithgps.com/routes/32978288"), 
+    ("Bla_c_kLivesMatter", "https://ridewithgps.com/routes/32979113"), 
+    ("Blac_k_LivesMatter", "https://ridewithgps.com/routes/33078801"), 
+    ("Black_L_ivesMatter", "https://ridewithgps.com/routes/33078890"), 
+    ("BlackL_i_vesMatter", "https://ridewithgps.com/routes/33176643"), 
+    ("BlackLi_v_esMatter", "https://ridewithgps.com/routes/32979727"), 
+    ("BlackLiv_e_sMatter", "https://ridewithgps.com/routes/33076433"), 
+    ("BlackLive_s_Matter", "https://ridewithgps.com/routes/33076537"), 
+    ("BlackLives_M_atter", "https://ridewithgps.com/routes/33076805"), 
+    ("BlackLivesM_a_tter", "https://ridewithgps.com/routes/33076918"), 
+    ("BlackLivesMa_t_ter", "https://ridewithgps.com/routes/33077009"), 
+    ("BlackLivesMat_t_er", "https://ridewithgps.com/routes/33077220"), 
+    ("BlackLivesMatt_e_r", "https://ridewithgps.com/routes/33077727"), 
+    ("BlackLivesMatte_r_", "https://ridewithgps.com/routes/33077542"), 
+]
+
+for name, route in routes:
+    os.system(f'curl -o {name}.tcx {route}.tcx')
