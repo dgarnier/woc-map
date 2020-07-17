0add0b84bbf3c1411b0d3324d6e22e5b05c0c24f
Darren Garnier <dgarnier@reinrag.net>
2020-07-17 02:25:31 -0400

    admin functions page

--- app/utils.py
+++ app/utils.py
@@ -3,20 +3,17 @@ import csv
 from flask import send_file
 
 
-def cvsfileify(dict_list, filename):
+def cvsfileify(dict_list, keys, filename):
 
     output = io.StringIO()
-    keys = list(dict_list[0])
-    keys.remove('resource_state')
     writer = csv.DictWriter(output, keys,
                             extrasaction='ignore', dialect=csv.excel)
     writer.writeheader()
-    writer.writerows(dict_list)
+    for d in dict_list:
+        writer.writerow(d)
     csv_string = output.getvalue()
     mem = io.BytesIO(csv_string.encode('utf-8'))
-    return send_file(
-        mem,
-        as_attachment=True,
-        attachment_filename=filename,
-        mimetype='text/csv'
-    )
+    return send_file(mem, as_attachment=True,
+                     attachment_filename=filename,
+                     mimetype='text/csv'
+                     )
